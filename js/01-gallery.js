894841a344ab194abaf132cf5900b54ad1066bbf
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-07-31 09:10:28 +0200

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+for (let i = 0; i < galleryItems.length; i++) { 
+
+const galleryHtml = `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItems[i].original}">
+    <img
+      class="gallery__image"
+      src="${galleryItems[i].preview}"
+      data-source="${galleryItems[i].original}"
+      alt="${galleryItems[i].description}"
+    />
+  </a>
+</div>`
+
+    gallery.innerHTML += galleryHtml;
+    
+}
+
+
+   
 
