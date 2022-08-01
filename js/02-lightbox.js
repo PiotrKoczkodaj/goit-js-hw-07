0cba6ebac219c42fbf891d4aecd467efd0e35f88
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2022-08-01 12:55:13 +0200

    .

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+for (let i = 0; i < galleryItems.length; i++) { 
+
+    const galleryHtml = `<a class="gallery__item" href="${galleryItems[i].original}">
+  <img class="gallery__image" src="${galleryItems[i].preview}" alt="${galleryItems[i].description}" />
+</a>`
+   
+    gallery.innerHTML += galleryHtml;
+
+}
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
